d190dac890dc24b38f7b804ebd96101378551cd7
matheusivi <matheus-willliam@hotmail.com>
2025-07-13 14:59:16 -0400

    adiciona cards dos filmes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,126 @@
+import { Button } from "@/components/ui/button";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 import Image from "next/image";
+
+const movies = [
+  {
+    title: "Homem de Ferro",
+    year: 2023,
+    genre: "Action",
+    duration: "2h 15m",
+    poster:
+      "https://postergami.com/wp-content/uploads/2022/01/poster-wallpaper-homem-de-ferro-1.jpg",
+    description: "Esta aventura começa...",
+  },
+  {
+    title: "Star Wars",
+    year: 2023,
+    genre: "Action",
+    duration: "3h 15m",
+    poster:
+      "https://th.bing.com/th/id/R.d9084abb1684ab72a5b246af0a3cdbb7?rik=kuB4LUX%2fezwVcg&pid=ImgRaw&r=0",
+    description: "Esta aventura começa...",
+  },
+  {
+    title: "Homem de Ferro",
+    year: 2023,
+    genre: "Action",
+    duration: "2h 15m",
+    poster:
+      "https://postergami.com/wp-content/uploads/2022/01/poster-wallpaper-homem-de-ferro-1.jpg",
+    description: "Esta aventura começa...",
+  },
+  {
+    title: "Homem de Ferro",
+    year: 2023,
+    genre: "Action",
+    duration: "2h 15m",
+    poster:
+      "https://postergami.com/wp-content/uploads/2022/01/poster-wallpaper-homem-de-ferro-1.jpg",
+    description: "Esta aventura começa...",
+  },
+  {
+    title: "Homem de Ferro",
+    year: 2023,
+    genre: "Action",
+    duration: "2h 15m",
+    poster:
+      "https://postergami.com/wp-content/uploads/2022/01/poster-wallpaper-homem-de-ferro-1.jpg",
+    description: "Esta aventura começa...",
+  },
+  {
+    title: "Homem de Ferro",
+    year: 2023,
+    genre: "Action",
+    duration: "2h 15m",
+    poster:
+      "https://postergami.com/wp-content/uploads/2022/01/poster-wallpaper-homem-de-ferro-1.jpg",
+    description: "Esta aventura começa...",
+  },
+  {
+    title: "Homem de Ferro",
+    year: 2023,
+    genre: "Action",
+    duration: "2h 15m",
+    poster:
+      "https://postergami.com/wp-content/uploads/2022/01/poster-wallpaper-homem-de-ferro-1.jpg",
+    description: "Esta aventura começa...",
+  },
+  {
+    title: "Homem de Ferro",
+    year: 2023,
+    genre: "Action",
+    duration: "2h 15m",
+    poster:
+      "https://postergami.com/wp-content/uploads/2022/01/poster-wallpaper-homem-de-ferro-1.jpg",
+    description: "Esta aventura começa...",
+  },
+  {
+    title: "Homem de Ferro",
+    year: 2023,
+    genre: "Action",
+    duration: "2h 15m",
+    poster:
+      "https://postergami.com/wp-content/uploads/2022/01/poster-wallpaper-homem-de-ferro-1.jpg",
+    description: "Esta aventura começa...",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <nav>
+        <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+          MatheusFlix
+        </h1>
+      </nav>
+      <div className="grid grid-flow-col grid-rows-3 gap-4">
+        {movies.map((movie, index) => (
+          <Card
+            key={index}
+            className="w-full bg-gray-900 text-white rounded-lg shadow-lg"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <CardHeader className="p-0">
+              <Image
+                src={movie.poster}
+                alt={`${movie.title}`}
+                width={256}
+                height={384}
+                className="w-full h-96 object-cover rounded-t-lg"
+              />
+            </CardHeader>
+            <CardContent className="p-4">
+              <CardTitle className="text-lg font-bold">{movie.title}</CardTitle>
+              <p className="text-sm text-gray-400">
+                {movie.year} • {movie.genre} • {movie.duration}
+              </p>
+              <p className="mt-2 text-sm">{movie.description}</p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
