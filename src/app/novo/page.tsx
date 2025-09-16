b86d04bc86148d3b1650b830c60d8877fcbc1e3a
Matheus William <104685233+matheusivi@users.noreply.github.com>
2025-09-15 20:11:16 -0400

    add teste

--- src/app/novo/page.tsx
+++ src/app/novo/page.tsx
@@ -37,7 +37,7 @@ export default function AdicionarFilme() {
       !form.description ||
       !form.rating
     ) {
-      alert("Por favor, preencha todos os campos.");
+      alert("Por favor, preencha todos os campos!.");
       return;
     }
 
